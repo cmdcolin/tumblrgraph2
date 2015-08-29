dbee8b7d3364b006816dec9d2a439f031941d924
Colin <colin.diesh@gmail.com>
2015-08-29 16:03:35 -0500

    Add layouts

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,7 @@ set u
 rm -rf dist
 mkdir -p dist/js
 mkdir -p dist/img
-browserify js/index.js | uglifyjs > dist/js/index.js
+browserify index.js | uglifyjs > dist/index.js
 cp index.html dist/index.html
 cp js/*.js dist/js/
 cp index.html dist
