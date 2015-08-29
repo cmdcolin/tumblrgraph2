ef9b162a02713173dbb512e43d755dc522b1e64a
Colin <colin.diesh@gmail.com>
2015-08-29 14:17:46 -0500

    Setup springy layout and intial example

--- js/index.js
+++ js/index.js
@@ -1,81 +1,73 @@
 var cytoscape=require('cytoscape');
-var tools=require('./accessories');
 var domready=require('domready');
 
-domready(function(){ // on dom ready
-
-    var cytoscape_style= cytoscape.stylesheet()
-    .selector('node')
-      .css({
-        'content': 'data(name)',
-        'text-valign': 'center',
-        'color': 'white',
-        'text-outline-width': 2,
-        'text-outline-color': '#888'
-      })
-    .selector('edge')
-      .css({
-        'target-arrow-shape': 'triangle'
-      })
-    .selector(':selected')
-      .css({
-        'background-color': 'black',
-        'line-color': 'black',
-        'target-arrow-color': 'black',
-        'source-arrow-color': 'black'
-      })
-    .selector('.faded')
-      .css({
-        'opacity': 0.25,
-        'text-opacity': 0
-      });
-
-
-    var nodes={};
-    var edges={};
-    document.querySelector("#myform").addEventListener("submit", function(e){
-        e.preventDefault();    //stop form from submitting
-        var lines=document.querySelector("#notes").value.split("\n");
-        lines.forEach(function(line) {
-            var matches;
-            if(matches=line.match(/(\S+) reblogged this from (\S+)/)) {
-                if(!nodes[matches[1]]) nodes[matches[1]]={id: matches[1]};
-                if(!nodes[matches[2]]) nodes[matches[2]]={id: matches[2]};
-                if(!edges[matches[1]+','+matches[2]]) edges[matches[1]+','+matches[2]]={source:matches[1],target:matches[2]};
-            }
-        });
-
-        var nodes_cy=[];
-        var edges_cy=[];
-        Object.keys(nodes).forEach(function(node) {
-            nodes_cy.push({data:nodes[node]});
-        });
-        Object.keys(edges).forEach(function(edge) {
-            edges_cy.push({data:edges[edge]});
-        });
-        console.log(nodes_cy);
-        console.log(edges_cy);
-
-        cytoscape({
-          container: document.getElementById('cy'),
-          style: cytoscape_style,
-          elements: {
-              "nodes": nodes_cy,
-              "edges": edges_cy
-          },
-          layout: {
-            name: 'springy',
-            padding: 10,
-            randomize:false 
-          },
-          ready: function(){
-            var cy=this;
-            cy.panningEnabled(false);
-            cy.zoomingEnabled(false);
-          }
-        });
+domready(function(){
+  var cytoscape_style= cytoscape.stylesheet()
+  .selector('node')
+    .css({
+      'content': 'data(name)',
+      'text-valign': 'center',
+      'color': 'white',
+      'text-outline-width': 2,
+      'text-outline-color': '#888'
+    })
+  .selector('edge')
+    .css({
+      'target-arrow-shape': 'triangle'
+    })
+  .selector(':selected')
+    .css({
+      'background-color': 'black',
+      'line-color': 'black',
+      'target-arrow-color': 'black',
+      'source-arrow-color': 'black'
+    })
+  .selector('.faded')
+    .css({
+      'opacity': 0.25,
+      'text-opacity': 0
     });
 
-//
-}); // on dom ready
+
+  var nodes={};
+  var edges={};
+  document.querySelector("#myform").addEventListener("submit", function(e){
+    e.preventDefault();
+    document.querySelector("#notes").value.split("\n").forEach(function(line) {
+      var matches;
+      if(matches=line.match(/(\S+) reblogged this from (\S+)/)) {
+        if(!nodes[matches[1]]) nodes[matches[1]]={id: matches[1], name: matches[1]};
+        if(!nodes[matches[2]]) nodes[matches[2]]={id: matches[2], name: matches[2]};
+        if(!edges[matches[1]+','+matches[2]]) edges[matches[1]+','+matches[2]]={source:matches[1],target:matches[2]};
+      }
+    });
+
+    var nodes_cy=[];
+    var edges_cy=[];
+    Object.keys(nodes).forEach(function(node) {
+      nodes_cy.push({data:nodes[node]});
+    });
+    Object.keys(edges).forEach(function(edge) {
+      edges_cy.push({data:edges[edge]});
+    });
+
+    cytoscape({
+      container: document.getElementById('cy'),
+      style: cytoscape_style,
+      elements: {
+        "nodes": nodes_cy,
+        "edges": edges_cy
+      },
+      layout: {
+         name: 'springy',
+         padding: 10
+      },
+      ready: function(){
+        var cy=this;
+        cy.panningEnabled(false);
+        cy.zoomingEnabled(false);
+      }
+    });
+  });
+});
 
