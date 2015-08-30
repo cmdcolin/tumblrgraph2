b11b81fdb4d31127d923f4ec60afa1eace9ba5bc
Colin <colin.diesh@gmail.com>
2015-08-29 19:15:21 -0500

    Use underscore for mapping instead of object.keys

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var cytoscape=require('cytoscape');
 var domready=require('domready');
+var _=require('underscore');
 
 domready(function(){
   var timer;
   function submitForm() {
+    // input nodes/edges for reblogs and OP
     var nodes={};
     var edges={};
+    var original_poster;
+
+    // user form
     var notes=$("#notes").val();
     var layout=$("#layout option:selected").text();
     var layout_time=$("#layout_time").val();
-    var poster;
+
+    // clear the timer for stopping animation
     clearTimeout(timer);
-    
+   
+
+    // process textarea from form
     notes.split("\n").forEach(function(line) {
       var matches;
-      
+     
+      // get reblogs
       if(matches=line.match(/(\S+) reblogged this from (\S+)/)) {
         if(!nodes[matches[1]]) nodes[matches[1]]={id: matches[1], name: matches[1], score:1};
         else nodes[matches[1]].score+=5;
@@ -23,26 +32,28 @@ domready(function(){
         if(!edges[matches[1]+','+matches[2]]) edges[matches[1]+','+matches[2]]={source:matches[1],target:matches[2]};
       }
 
+      // get original poster
       if(matches=line.match(/(\S+) posted this/)) {
-        poster=matches[1];
+        original_poster=matches[1];
       }
     });
 
-    var nodes_cy=[];
-    var edges_cy=[];
-    Object.keys(nodes).forEach(function(node) {
-      nodes_cy.push({data:nodes[node]});
+    var nodes_cy=_.map(nodes, function(node) {
+      return {"data":node};
     });
-    Object.keys(edges).forEach(function(edge) {
-      edges_cy.push({data:edges[edge]});
+    var edges_cy=_.map(edges,function(edge) {
+      return {"data":edge};
     });
+    console.log(edges_cy);
+    console.log(nodes_cy);
 
+
+    // set boring stylesheet
     var stylesheet_cy=cytoscape.stylesheet()
       .selector('node')
         .style({
           'content': 'data(name)',
           'text-valign': 'center',
-          'background-color': function(elt) { return 'hsl('+elt.data('score')+',80%,55%)'; },
           'text-outline-width': 2,
           'text-outline-color': '#000',
           'color': '#fff'
@@ -50,19 +61,19 @@ domready(function(){
       .selector('edge')
         .css({
           'target-arrow-shape': 'triangle'
-        })
-    
+        });
+   
+    // create cytoscape instance
     var cy=cytoscape({
       container: document.getElementById('cy'),
       style: stylesheet_cy,
       elements: {
         "nodes": nodes_cy,
         "edges": edges_cy
-      },
-      ready: function(){
-        var cy=this;
       }
     });
+
+    //manually crate and stop layout after timeout
     var layout_cy=cy.makeLayout({
       name: layout,
       padding: 10,
@@ -74,6 +85,8 @@ domready(function(){
 
     layout_cy.run();
 
+
+    // update status
     $("#status").text("Optimizing layout");
     timer=setTimeout(function() {
       layout_cy.stop();
@@ -81,23 +94,26 @@ domready(function(){
     }, layout_time);
 
 
-    console.log($("#color_by_bfs").val())
 
+    // color by distance from original poster using BFS
     if($("#color_by_bfs").val()=="on") {
       var max_depth=1;
-      var bfs = cy.elements().bfs('#'+poster, function(i, depth){
+      var bfs = cy.elements().bfs('#'+original_poster, function(i, depth){
         if(depth>max_depth) { max_depth=depth; }
       }, false);
 
       //use breadth first search to color nodes
-      var bfs = cy.elements().bfs('#'+poster, function(i, depth){
+      var bfs = cy.elements().bfs('#'+original_poster, function(i, depth){
         this.style('background-color', 'hsl('+depth*150/max_depth+',80%,55%)');
       }, false);
     }
   }
-  document.querySelector("#myform").addEventListener("submit", function(e){
-    e.preventDefault();
+
+
+  // resubmit form
+  $("#myform").submit(function(e){
     submitForm();
+    return false;
   });
   submitForm();
 
